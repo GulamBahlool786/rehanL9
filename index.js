b2bc56bb6bddcd9dc8eb14b7871c81dff00f5c84
GulamBahlool786 <117851487+GulamBahlool786@users.noreply.github.com>
2023-01-18 20:53:43 +0530

    L9 commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(port, function(){
   console.log(`server listening at port - ${port}`);
 });
